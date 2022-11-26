6727cbdbc5fa5c7714dc3dac116573617ad74980
Gurfy Person <cbates8923@student.pps.net>
2022-11-26 13:47:33 -0800

    Added basic physics to boss, so that it can be knocked back. Probably should add some dopamine in soon

--- OGPC_Project_Native_Directory/InfiniteHealthBoss.gd
+++ OGPC_Project_Native_Directory/InfiniteHealthBoss.gd
@@ -3,7 +3,13 @@ extends KinematicBody2D
 
 var attacking = false
 var was_attacking = false
+var was_attacked = false
+var velocity = Vector2.ZERO
+
 var attack_frames_length
+
+export var gravity_strength = 10
+export var friction_strength = 20
 
 
 func _ready():
@@ -11,6 +17,12 @@ func _ready():
 	attack_frames_length = len($AnimatedSprite.frames.frames)
 
 func _physics_process(delta):
+	Apply_Gravity()
+	Apply_Friction()
+	
+	if self.was_attacked:
+		velocity += Vector2(10, -5)
+	
 	if attacking:
 		$AnimatedSprite.animation = "attacking"
 		$AnimatedSprite.play() 
@@ -24,7 +36,15 @@ func _physics_process(delta):
 	if $AnimatedSprite.frame == attack_frames_length + 2:
 		attacking = false
 		
+	position += velocity * delta
+		
 	var was_attacking = attacking
+	
+func Apply_Gravity():
+	velocity.y += gravity_strength
+	
+func Apply_Friction():
+	velocity.x = move_toward(velocity.x, 0, friction_strength)
 
 func _on_AttackTimer_timeout():
 	attacking = true
@@ -33,4 +53,5 @@ func _on_AttackTimer_timeout():
 func on_knockback_event():
 	$AnimatedSprite.animation = "damaged"
 	attacking = false
+	was_attacked = true 
  
