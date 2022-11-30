61277b9ced8af3e856e85ab4bf3373961c4b0132
Gurfy Person <cbates8923@student.pps.net>
2022-11-29 19:22:00 -0800

    Implemented basic healthbar

--- OGPC_Project_Native_Directory/InfiniteHealthBoss.gd
+++ OGPC_Project_Native_Directory/InfiniteHealthBoss.gd
@@ -5,26 +5,33 @@ var attacking = false
 var was_attacking = false
 var was_attacked = false
 var velocity = Vector2.ZERO
+var died = false
 
+var one_health_unit_in_image_scale
 var attack_frames_length
 
 export var gravity_strength = 10
 export var friction_strength = 20
-
-signal boss_hit
+export var health = 100 # Can be changed
 
 
 func _ready():
 	$AttackTimer.start()
 	attack_frames_length = len($AnimatedSprite.frames.frames)
+	one_health_unit_in_image_scale = $Healthbar_Health.scale.x / health
 
 func _physics_process(delta):
 	Apply_Gravity()
 	Apply_Friction()
 	
+	$Healthbar_Health.scale.x = one_health_unit_in_image_scale * health
+	
+	if died:
+		$Healthbar_Health.scale.x = one_health_unit_in_image_scale
+	
 	if was_attacked:
 		was_attacked = false
-		velocity = Vector2(100, -100)
+		velocity = Vector2(100, -30)
 	
 	if attacking:
 		$AnimatedSprite.animation = "attacking"
@@ -55,6 +62,9 @@ func _on_AttackTimer_timeout():
 # In the future this will be and connection from the player bullet when it damages the enemy.
 func on_Knockback_event():
 	#$AnimatedSprite.animation = "damaged"
+	health -= 1
+	if health <= 0:
+		died = true
 	attacking = false
 	was_attacked = true 
  
