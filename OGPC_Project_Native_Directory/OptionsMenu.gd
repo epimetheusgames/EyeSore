32682c1d7a413784f91f202e467b379645378376
Gurfy Person <cbates8923@student.pps.net>
2022-12-14 16:23:55 -0800

    Well ... it's a tileset

---
+++ OGPC_Project_Native_Directory/OptionsMenu.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var selected = 0
+onready var options = [$Label, $Label2, $Label3, $Label4, $Label5]
+
+func _process(delta):
+	for option in range(len(options)):
+		if option == selected:
+			options[option].modulate = Color(2.75, 2.75, 2.75, 1)
+		else:
+			print(options[option])
+			options[option].modulate = Color(1, 1, 1, 1)
+		
+	if Input.is_action_just_pressed("Just_Arrowkey_Down") and selected < 4:
+		selected += 1
+	if Input.is_action_just_pressed("Just_Arrowkey_Up") and selected > 0:
+		selected -= 1
+		
+	if Input.is_action_just_pressed("ui_accept"):
+		if selected == 4:
+			get_tree().change_scene_to(load("res://MainMenu.tscn"))
