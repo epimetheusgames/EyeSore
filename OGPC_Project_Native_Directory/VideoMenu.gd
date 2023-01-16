062e3be82ae311a8b8fc0c4cb24524c48bcbecb5
Gurfy Person <cbates8923@student.pps.net>
2023-01-16 15:23:03 -0800

    Crudely added a brightness functionality (yummy rectangle), unsaved, and sliders are broken

--- OGPC_Project_Native_Directory/VideoMenu.gd
+++ OGPC_Project_Native_Directory/VideoMenu.gd
@@ -4,3 +4,5 @@ extends Node2D
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		get_parent().Open_Options_Menu(self)
+	
+	get_parent().Set_Screen_Brightness($Slider.get_value()/100)
