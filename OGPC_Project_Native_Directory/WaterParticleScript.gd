d4a4fc99abdb41725fce414a0327f200c9b7eddd
noah <brightblue3d@gmail.com>
2023-02-02 20:12:52 -0800

    particles now remove tiles Yay!

--- OGPC_Project_Native_Directory/WaterParticleScript.gd
+++ OGPC_Project_Native_Directory/WaterParticleScript.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 var collision_point = Vector2(0, 0)
 var tile_coordinates = Vector2(0, 0)
+var tile_pos
 
 func _ready():
 	contact_monitor = true 
@@ -14,17 +15,19 @@ func _process(delta):
 		# if the colliding body is not another water particle, or the player, run the code inside
 		if not "Water_Particle" in body.name and body.name != "Player_Body":
 			# delete this water particle
-			queue_free()
+			self.queue_free()
 
 
 func _on_RigidBody2d_body_entered(body):
 	# if the node the water particle collides with is a TileMap, run the code inside
 	if "TileMap" in body.name:
 		# get the TileMap Node
-		var tile_map = body.get_node("/root/Ground_Tilemap")
-		# get the cell coordinates in world coordinates
-		collision_point = body.get_collision_point(0)
-		# convert the world coordinates to TileMap coordinates
-		tile_coordinates = tile_map.world_to_map(collision_point)
-		# set the cell at those coordinates to 0, thus deleting it, in the future will switch it to a different tile in the TileSet
-		tile_map.set_cell(tile_coordinates.x, tile_coordinates.y - 1, 0)
+		var tile_map = body
+		
+		var mask = tile_map.get_collision_mask()
+		
+		if mask > 0:
+			tile_pos = tile_map.world_to_map(self.position)
+			tile_map.set_cell(tile_pos.x, tile_pos.y, -1)
+			tile_map.update_bitmask_area(tile_pos)
+			self.queue_free()
