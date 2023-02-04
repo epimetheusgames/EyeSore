6c8bd3fd6329c1f3f89d78e4eb71b69c1ae2ce17
Gurfy Person <cbates8923@student.pps.net>
2023-02-03 21:15:44 -0800

    push2pull.com

--- OGPC_Project_Native_Directory/Levels/Level_Script.gd
+++ OGPC_Project_Native_Directory/Levels/Level_Script.gd
@@ -35,11 +35,21 @@ func loading_edge_case_handler(node, list, item): # Handle edge cases that may h
 			return 1 # Break out of the loop if it's not an unwanted item because those are already
 			# loaded in. At least that keyword should be triggered outside this function
 	return 0
-
-func set_player_spawnpoint_and_position_reality(health, player_position, spawnpoint, enemy1s, enemy2s, pixelated_bosses, tree):
+	
+func set_player_spawnpoint_and_position(healthp, player_positionp, spawnpointp, enemy1sp, enemy2sp, pixelated_bossesp):
 	# This function is responsible for setting all of that stuff that the player would
 	# Explode if they didn't have. I know right remove one line of code and you could 
-	# Reset your health just by clicking exit to menu. Crazy that I didn't let that happen.
+	# Reset your health just by clicking exit to menu. Lol that I didn't let that happen.
+	health = healthp
+	player_position = player_positionp 
+	spawnpoint = spawnpointp
+	enemy1s = enemy1sp 
+	enemy2s = enemy2sp 
+	pixelated_bosses = pixelated_bossesp
+
+func set_player_spawnpoint_and_position_reality(health, player_position, spawnpoint, enemy1s, enemy2s, pixelated_bosses, tree):
+	# The function above, but all those crazy exploits are fixed but I wanted to keep
+	# that for reference. and other functions break without it so I'll just keep it.
 	$Save_Functionality/Player_Body.respawn_position = spawnpoint
 	$Save_Functionality/Player_Body.position = player_position
 	
@@ -51,7 +61,6 @@ func set_player_spawnpoint_and_position_reality(health, player_position, spawnpo
 	var enemy1s_list = convert2list(enemy1s)
 	var enemy2s_list = convert2list(enemy2s)
 	
-	# Okay so zombies
 	for enemy1_node in range(len(enemy1_nodes)):
 		var item = enemy1_nodes[enemy1_node]
 		var status = loading_edge_case_handler(enemy1_node, enemy1s_list, item)
@@ -64,7 +73,6 @@ func set_player_spawnpoint_and_position_reality(health, player_position, spawnpo
 		item.position.x = enemy1s_list[enemy1_node]["position_x"] # Set the node's x and y position to the 
 		item.position.y = enemy1s_list[enemy1_node]["position_y"] # saved data.
 	
-	# those flyey guys.
 	for enemy2_node in range(len(enemy2_nodes)):
 		var item = enemy2_nodes[enemy2_node]
 		var status = loading_edge_case_handler(enemy2_node, enemy2s_list, item)
@@ -81,15 +89,10 @@ func set_player_spawnpoint_and_position_reality(health, player_position, spawnpo
 		item.point2.x = enemy2s_list[enemy2_node]["end_position_x"]
 		item.point2.y = enemy2s_list[enemy2_node]["end_position_y"]
 		
-	# Boom!
-	
 func _process(delta):
-	# Open the pause menu if that happens.
 	if Input.is_action_just_pressed("ui_pause"):
 		get_parent().Open_Pause_Menu()
 	
-	# Load the game if we haven't.
 	if not loaded:
 		loaded = true 
-		# Reality because that older function that is gone now ):
 		set_player_spawnpoint_and_position_reality(health, player_position, spawnpoint, enemy1s, enemy2s, pixelated_bosses, get_tree())
