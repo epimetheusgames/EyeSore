89e75f51513036b65eab1f1001f58d506b44aa72
Gurfy Person <blackhole243243@gmail.com>
2023-03-18 13:37:31 -0700

    Moveable block can now push buttons as shown in PortalTestLevel.tscn. Portals are now ready to be used in levels

--- OGPC_Project_Native_Directory/Button.gd
+++ OGPC_Project_Native_Directory/Button.gd
@@ -7,17 +7,24 @@ func _ready():
 	add_to_group("buttons")
 
 func _on_Area2D_area_entered(area):
-	if $AnimatedSprite.animation == "Unpressing" and "Player" in area.name:
+	print(area.name)
+	if $AnimatedSprite.animation == "Unpressing" and ("Player" in area.name or "MoveableBlock" in area.name):
 		$AnimatedSprite.animation = "Pressing"
 		$AnimatedSprite.play()
 		Player_Manage_Wires()
 
 func _on_Area2D_area_exited(area):
-	if $AnimatedSprite.animation == "Pressing" and "Player" in area.name:
+	if $AnimatedSprite.animation == "Pressing" and ("Player" in area.name or "MoveableBlock" in area.name):
 		$AnimatedSprite.animation = "Unpressing"
 		$AnimatedSprite.play()
 		if not toggle:
 			Player_Manage_Wires()
+			
+func _on_Area2D_body_entered(body):
+	_on_Area2D_area_entered(body)
+
+func _on_Area2D_body_exited(body):
+	_on_Area2D_area_exited(body)
 
 func Player_Manage_Wires():
 	var grass_tileset = get_parent().get_node("TileMap")
@@ -25,4 +32,5 @@ func Player_Manage_Wires():
 	var death_pos_on_tileset = tileset.world_to_map(position + $Area2D.scale / 2)
 	
 	get_parent().get_node("Player_Body").manage_wires(death_pos_on_tileset, tileset, grass_tileset)
+
 
