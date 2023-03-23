a6b3e62101286be4bd8f07e86d499f9735bffeea
Gurfy Person <cbates8923@student.pps.net>
2023-03-22 19:12:07 -0700

    Added the slow release button (See in PuzzleLevel4) and started working on another puzzle I guess (Again, PuzzleLevel4)

--- OGPC_Project_Native_Directory/Button.gd
+++ OGPC_Project_Native_Directory/Button.gd
@@ -1,24 +1,23 @@
 extends Node2D
 
 export var toggle = false
-
+var finished = false
+var first_time = true
 
 func _ready():
 	add_to_group("buttons")
 
 func _on_Area2D_area_entered(area):
 	print(area.name)
-	if $AnimatedSprite.animation == "Unpressing" and ("Player" in area.name or "MoveableBlock" in area.name):
+	if $AnimatedSprite.animation == "Unpressing" and finished and ("Player" in area.name or "MoveableBlock" in area.name):
 		$AnimatedSprite.animation = "Pressing"
 		$AnimatedSprite.play()
-		Player_Manage_Wires()
 
 func _on_Area2D_area_exited(area):
 	if $AnimatedSprite.animation == "Pressing" and ("Player" in area.name or "MoveableBlock" in area.name):
+		finished = false
 		$AnimatedSprite.animation = "Unpressing"
 		$AnimatedSprite.play()
-		if not toggle:
-			Player_Manage_Wires()
 			
 func _on_Area2D_body_entered(body):
 	_on_Area2D_area_entered(body)
@@ -33,4 +32,12 @@ func Player_Manage_Wires():
 	
 	get_parent().get_node("Player_Body").manage_wires(death_pos_on_tileset, tileset, grass_tileset)
 
-
+func _on_AnimatedSprite_animation_finished():
+	finished = true
+	if $AnimatedSprite.animation == "Unpressing":
+		if not toggle and not first_time:
+			Player_Manage_Wires()
+		elif first_time:
+			first_time = false
+	elif $AnimatedSprite.animation == "Pressing":
+		Player_Manage_Wires()
