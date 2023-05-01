ab8cfa6d86fabb695a78b47f7a0f21dfa39b5783
Gurfy Person <cbates8923@student.pps.net>
2023-04-30 17:54:44 -0700

    Multiple updates to keybinds
    	- Switched bullet and shockwave (they are pretty much ambiguous)
    	  to respawn and wire ui access (r and e defaults)
    	- Game now COMPLETELY restarts when exiting keybinds menu, to fix bugs that arised
    	  before (including two different buttons for one action).
    	- Hardcoded keybind names for up, down, left, and right.

--- OGPC_Project_Native_Directory/ControlsMenu.gd
+++ OGPC_Project_Native_Directory/ControlsMenu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept") and not $Left.selected and not $Right.selected and not $Jump.selected and not $Shockwave.selected and not $Bullet.selected:
+	if Input.is_action_just_pressed("ui_accept") and not $Left.selected and not $Right.selected and not $Jump.selected and not $Terminal.selected and not $Respawn.selected:
 		exit_menu()
 	
 func clear_all_except(node):
@@ -19,13 +19,15 @@ func exit_menu():
 			"right": $Right.keybind, 
 			"jump-type": $Jump.keybind_type, 
 			"jump": $Jump.keybind,
-			"shockwave-type": $Shockwave.keybind_type,
-			"shockwave": $Shockwave.keybind, 
-			"bullet-type": $Bullet.keybind_type,
-			"bullet": $Bullet.keybind,
+			"terminal-type": $Terminal.keybind_type,
+			"terminal": $Terminal.keybind, 
+			"respawn-type": $Respawn.keybind_type,
+			"respawn": $Respawn.keybind,
 		}
 	)
 	$SaveFunctionality.save_game()
+	get_tree().paused = false
+	get_tree().reload_current_scene()
 	get_parent().Open_Options_Menu(self)
 
 func _on_TextureButton_button_up():
