2c31909ef0eb7a6b961d93d881c46dbb6c512617
Gurfy Person <cbates8923@student.pps.net>
2023-05-06 12:14:35 -0700

    Made wire access a little more accessible, also changed wire key to interact key

--- OGPC_Project_Native_Directory/Wire_Terminal.gd
+++ OGPC_Project_Native_Directory/Wire_Terminal.gd
@@ -1,35 +1,45 @@
 extends Node2D
 
-export (float, 1, 1000) var frequency = 5
-export (float, 1000) var amplitude = 150
-var time = 0
-
 var is_in_use_range = false
 var one_time_no_use = 1
 
 
 func _ready():
 	$Terminal_Background_Sprite.hide()
+	$CanvasLayer/Press2Use.hide()
 	self.add_to_group("Wire_Terminal")
 
 func _on_Player_Use_Range_body_entered(body):
 	if one_time_no_use:
 		one_time_no_use -= 1
 		return 
+		
+	if not "Player_Body" in body.name:
+		return
 	
 	$Terminal_Background_Sprite.show()
+	$CanvasLayer/Press2Use.show()
 	is_in_use_range = true
 
 func _on_Player_Use_Range_body_exited(body):
+	if not "Player_Body" in body.name:
+		return
+	
 	$Terminal_Background_Sprite.hide()
+	$CanvasLayer/Press2Use.hide()
 	is_in_use_range = false
 
+func Get_Current_Camera():
+	var viewport = get_viewport()
+	if not viewport:
+		return null
+	var camerasGroupName = "__cameras_%d" % viewport.get_viewport_rid().get_id()
+	var cameras = get_tree().get_nodes_in_group(camerasGroupName)
+	for camera in cameras:
+		if camera is Camera2D and camera.current:
+			return camera
+	return null
+
 func _physics_process(delta):
-	time += delta
-	var movement = cos(time * frequency) * amplitude
-	$Btn_Prompt_Spr.position.y += (movement * delta)
-	
-	if is_in_use_range:
-		$Btn_Prompt_Spr.visible = true 
-	else:
-		$Btn_Prompt_Spr.visible = false
+	$CanvasLayer/Press2Use.rect_position = Get_Current_Camera().position / Get_Current_Camera().zoom - Vector2(get_viewport().size.x / 2, -get_viewport().size.y / 2) + Vector2($CanvasLayer/Press2Use.rect_size.x / 1.5, -10)
+
